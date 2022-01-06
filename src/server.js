6c18009e53ac9f202f2e7d06cce51b44601fd078
Monica Veiga Puente <monica.veigapuente@plexus.es>
2022-01-06 10:59:51 +0100

    preparing app for heroku

--- src/server.js
+++ src/server.js
@@ -3,9 +3,12 @@ const path = require('path');
 
 const app = express();
 
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/safetravels'));
-app.get('/',function(req,res){
-    res.sendFile(path.join('index.html', {root: 'dist/safetravels'}));
-});
 
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/safetravels/'}),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
